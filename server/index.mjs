9b9cd796bc15201021d09422f74e846918cf81ac
Ezequiel Miranda <ezequiel.miranda@adevinta.com>
2023-11-09 11:11:44 +0100

    aws auth

--- server/index.mjs
+++ server/index.mjs
@@ -13,9 +13,11 @@ app.use(express.json())
 app.use(cors())
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  }
 })
 
 const model = new BedrockChat({
